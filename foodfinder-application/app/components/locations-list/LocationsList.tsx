3c6920e8c7e4503f9b4182e020a1d0cfd6888755
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2025-02-14 23:52:46 +0400

    ✨ S5: create LocationsList component

---
+++ foodfinder-application/app/components/locations-list/LocationsList.tsx
@@ -0,0 +1,28 @@
+import { LocationType } from "@/mongoose/locations/schema";
+import styled from "styled-components";
+import { JSX } from "react";
+import { LocationsListItem } from "../locations-list-items/LocationsListItem";
+
+interface PropsInterface {
+    locations: LocationType[];
+}
+
+export const LocationsList = ( props : PropsInterface): JSX.Element => {
+        return (
+            <LocationListStyled>
+              {props.locations.map((location) => {
+                return (
+                    <LocationsListItem
+                        location={location}
+                        key={location.location_id}
+                    />
+                );
+            })}
+            </LocationListStyled>
+        );
+    };
+
+const LocationListStyled = styled.ul`
+    margin: 0;
+    padding: 0;
+`
